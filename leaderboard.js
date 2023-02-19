7418159401359ae0f8e7144b63b7a2b406ede276
Vishwa V <90657995+VishwaV10@users.noreply.github.com>
2023-02-18 23:55:49 -0800

    Add files via upload

---
+++ leaderboard.js
@@ -0,0 +1,9 @@
+function getFromServer() {
+    let req = new XMLHttpRequest();
+
+    req.open("GET", "https://272.selfip.net/apps/hlvQ3jZ0kh/collections/usersTest/documents/")
+    req.send();
+    req.addEventListener("load", function() {
+        var listOfPpl = JSON.parse(req.responseText);
+    })
+}
---
+++ startPage.js
@@ -0,0 +1,4 @@
+function startPage() {
+    document.body.style.backgroundImage = "url('')";
+    document.body.style.backgroundPosition = "center";
+}
